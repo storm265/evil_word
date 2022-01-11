15bd8d60b6114ad752a0f903f4cf056bdb4a5e93
peter3374 <peter4533@mail.ru>
2022-01-11 22:43:25 +0300

    beta2

---
+++ lib/home.dart
@@ -0,0 +1,148 @@
+import 'dart:convert';
+import 'package:evil_word/decorations.dart';
+import 'package:evil_word/model/model.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:connectivity_plus/connectivity_plus.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // getting joke
+  Future<Joke> getData() async {
+    var responce = await http.get(Uri.parse(
+        'https://evilinsult.com/generate_insult.php?lang=en&type=json'));
+    if (responce.statusCode != 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text('Error of getting data.')));
+    }
+    if (responce.statusCode == 200) {
+      Map<String, dynamic> jsonInfo = json.decode(responce.body);
+      return Joke.fromJson(jsonInfo);
+    } else {
+      throw Exception('statusCode !=200');
+    }
+  }
+
+  var subscription;
+  @override
+  void initState() {
+    super.initState();
+    subscription = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult result) {
+      if (result == ConnectivityResult.mobile) {
+        print('mobile status');
+        getData();
+      } else if (result == ConnectivityResult.wifi) {
+        print('wifi status');
+        getData();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: const Text('Required internet connection!')));
+      }
+      print(result);
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    subscription.cancel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: UiDecoration.backgroundColor,
+      appBar: UiDecoration.appBar(Theme.of(context).colorScheme.secondary),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Center(
+              child: FutureBuilder<Joke>(
+                future: getData(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return CircularProgressIndicator();
+                  }
+
+                  if (snapshot.hasData) {
+                    final student = snapshot.data;
+
+                    return Material(
+                      elevation: 1,
+                      color: Colors.transparent,
+                      shadowColor: Theme.of(context).colorScheme.secondary,
+                      child: Container(
+                        padding: const EdgeInsets.all(7),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            border: Border.all(
+                                width: 3,
+                                color:
+                                    Theme.of(context).colorScheme.secondary)),
+                        child: Text(
+                          "${student!.insult}.",
+                          softWrap: true,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 25,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white),
+                        ),
+                      ),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
+                  }
+
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircularProgressIndicator(),
+                      const SizedBox(
+                        width: 8,
+                      ),
+                      Text(
+                        'Loading \😈',
+                        style: TextStyle(
+                            color: Theme.of(context).textTheme.headline6!.color,
+                            fontSize: 25),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+          // button
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Theme.of(context).colorScheme.secondary)),
+              onPressed: () {
+                setState(() {
+                  getData();
+                });
+              },
+              child: const Text(
+                'Generate joke',
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
-import 'dart:convert';
-
-import 'package:evil_word/info.dart';
+import 'package:evil_word/decorations.dart';
+import 'package:evil_word/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:http/http.dart' as http;
-import 'package:connectivity_plus/connectivity_plus.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,162 +10,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setEnabledSystemUIOverlays([]);
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Evil word',
+      title: '🤬 Evil word',
       theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        primarySwatch: Colors.red,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10))),
-          centerTitle: true,
-          title: const Text(
-            '🤬 Evil word',
-            style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 25,
-              color: Colors.white,
-            ),
-          ),
-        ),
-        backgroundColor: Colors.black26,
-        body: HomePage(),
-      ),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  Future<Joke> getData() async {
-    var responce = await http.get(Uri.parse(
-        'https://evilinsult.com/generate_insult.php?lang=en&type=json'));
-    if (responce.statusCode != 200) {
-      ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Error of getting data.')));
-    }
-    if (responce.statusCode == 200) {
-      Map<String, dynamic> jsonInfo = json.decode(responce.body);
-      return Joke.fromJson(jsonInfo);
-    } else {
-      throw Exception('statusCode !=200');
-    }
-  }
-
-  var subscription;
-  @override
-  void initState() {
-    super.initState();
-    subscription = Connectivity()
-        .onConnectivityChanged
-        .listen((ConnectivityResult result) {
-      if (result == ConnectivityResult.mobile) {
-        print('mobile status');
-        getData();
-      } else if (result == ConnectivityResult.wifi) {
-        print('wifi status');
-        getData();
-      } else {
-        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-            content: const Text('Required internet connection!')));
-      }
-      print(result);
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    subscription.cancel();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Center(
-            child: FutureBuilder<Joke>(
-              future: getData(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData) {
-                  return CircularProgressIndicator();
-                }
-                if (snapshot.hasData) {
-                  final student = snapshot.data;
-
-                  return Material(
-                    elevation: 1,
-                    color: Colors.transparent,
-                    shadowColor: Colors.redAccent,
-                    child: Container(
-                      padding: const EdgeInsets.all(7),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          border: Border.all(
-                              width: 3, color: Theme.of(context).accentColor)),
-                      child: Text(
-                        "${student!.insult}.",
-                        softWrap: true,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.white),
-                      ),
-                    ),
-                  );
-                } else if (snapshot.hasError) {
-                  return Text(snapshot.error.toString());
-                }
-
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircularProgressIndicator(),
-                    const SizedBox(
-                      width: 8,
-                    ),
-                    const Text(
-                      'Loading \😈',
-                      style: TextStyle(color: Colors.red, fontSize: 25),
-                    ),
-                  ],
-                );
-              },
-            ),
-          ),
-        ),
-        const SizedBox(
-          height: 10,
-        ),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {
-              getData();
-            });
-          },
-          child: const Text(
-            'Generate joke',
-            style: TextStyle(fontSize: 18),
-          ),
-          style: ButtonStyle(),
-        ),
-      ],
+          textTheme: TextTheme(
+              headline6:
+                  TextStyle(color: Theme.of(context).colorScheme.secondary)),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          colorScheme: ColorScheme.fromSwatch()
+              .copyWith(secondary: UiDecoration.mainColor)),
+      home: HomePage(),
     );
   }
 }
